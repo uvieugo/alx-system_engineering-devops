cf460d95988a86c6aa97a1c12cc0d4a391f763f0
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2023-04-03 23:03:54 +0100

    0x0F-load_balancer-0

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,17 @@
 # Installs a Nginx server with custom HTTP header
 
-exec {'update':
+exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
-}
-
-exec {'install Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
-}
-
-exec { 'add_header':
-  provider    => shell,
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf'
-  before      => Exec['restart Nginx'],
-}
-
-exec { 'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+  command  => '
+  sudo apt-get -y update;
+  sudo apt -y upgrade;
+  sudo apt-get -y install nginx;
+  sudo chown -R ubuntu:ubuntu /var/www;
+  echo "Hello World!" > /var/www/html/index.html;
+  echo "Ceci n'est pas une page" > /var/www/html/404.html;
+  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-enabled/default;
+  sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\n\terror_page 404 \/404.html;\n\tlocation = \/404.html {\n\t\troot \/var\/www\/html;\n\t\tinternal;\n\t}/" /etc/nginx/sites-enabled/default;
+  sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf;
+  sudo service nginx stop;
+  sudo service nginx start',
 }
